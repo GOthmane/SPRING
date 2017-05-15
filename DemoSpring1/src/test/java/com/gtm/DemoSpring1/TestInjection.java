64093dd21c11d971bb9b7bf66b794d70d7c0c9cf
GOthmane <guillaumeothmane@gmail.com>
2017-05-15 12:01:53 +0200

    Modification entete applicationContext.xml et test

---
+++ DemoSpring1/src/test/java/com/gtm/DemoSpring1/TestInjection.java
@@ -0,0 +1,19 @@
+package com.gtm.DemoSpring1;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestInjection {
+
+	public static void main(String[] args) {
+		
+		//1- Chargement du container et création des beans
+		ClassPathXmlApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//2- Récupération d'un bean
+		SocieteDevLogiciel societe = (SocieteDevLogiciel) appContext.getBean("societe");
+		//3- Traitement
+		System.out.println(societe);
+		//4- Destruction du container
+		appContext.close();
+	}
+
+}
