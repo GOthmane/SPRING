27fb205072a9c39eb26738eeebd505c8352cd4bc
GOthmane <guillaumeothmane@gmail.com>
2017-05-16 11:03:04 +0200

    Création classe user

---
+++ DemoSpring3/src/main/java/com/gtm/metier/User.java
@@ -0,0 +1,29 @@
+package com.gtm.metier;
+
+public class User {
+
+	private String nom;
+	private String prenom;
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	@Override
+	public String toString() {
+		return "User [nom=" + nom + ", prenom=" + prenom + "]";
+	}
+
+}
