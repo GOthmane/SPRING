52d1a90341f92d6bfff042d4dda8cc9310020f31
GOthmane <guillaumeothmane@gmail.com>
2017-05-18 10:59:48 +0200

    Mise en place AOP avec Spring (classe contenant aspects et web.xml)

---
+++ DemoSpring2/src/main/java/com/gtm/DemoSpring2/intercepteurs/Spectateurs.java
@@ -0,0 +1,18 @@
+package com.gtm.DemoSpring2.intercepteurs;
+
+public class Spectateurs {
+
+	public Spectateurs() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void  prendPlace(){
+		System.out.println("Les spectateurs prennent place");
+	}
+	
+	public void applaudit(){
+		System.out.println("Les spectateurs applaudissent");
+	}
+
+}
