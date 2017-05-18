86046e04ba7679437ed126cca5036bcc5d5675e2
GOthmane <guillaumeothmane@gmail.com>
2017-05-18 12:03:11 +0200

    Aspect around avec appel de la méthode en utilisant proceed()

--- DemoSpring2/src/main/java/com/gtm/DemoSpring2/intercepteurs/Musiciens.java
+++ DemoSpring2/src/main/java/com/gtm/DemoSpring2/intercepteurs/Musiciens.java
@@ -1,5 +1,7 @@
 package com.gtm.DemoSpring2.intercepteurs;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Musiciens {
 
 	public Musiciens() {
@@ -7,8 +9,15 @@ public class Musiciens {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public void jeJoue(){
+	// Méthode d'aspect around sans appeler la méthode cible
+//	public void jeJoue(){
+//		System.out.println("Je joue");
+//	}
+	
+	// Méthode d'aspect around avec appel de la méthode cible
+	public Object jeJoue(ProceedingJoinPoint pjp) throws Throwable{
 		System.out.println("Je joue");
+		return pjp.proceed();
 	}
 
 }
